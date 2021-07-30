612665de85cae789f1343fd4fd71a740e8185e18
Ramiro Mendez <ramiromendez@XIFRA-SIS006s-MacBook-Pro.local>
2021-07-30 13:33:45 -0500

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <section className="flex h-screen w-screen bg-gray-700 justify-center items-center">
+            <div className="text-center">
+                <h1 className="text-5xl font-semibold text-gray-400">Tailwind x React</h1>
+                <h2><a className="text-gray-500"
+                    href="https://github.com/RamiroG8k" target="_blank" rel="noreferrer">RamiroG8k</a> template</h2>
+            </div>
+        </section>
+    );
 }
 
 export default App;
