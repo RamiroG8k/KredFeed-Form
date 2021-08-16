ceab1769938c03d487ba8543cd737634ea053e97
Ramiro Mendez <ramiromendez@XIFRA-SIS006s-MacBook-Pro.local>
2021-08-16 10:23:15 -0500

    dark mode toggler

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,10 @@ function App() {
     }
 
     return (
-        <section className="flex flex-col h-screen w-screen bg-gray-700 justify-center items-center">
+        <section className="flex flex-col h-screen w-screen bg-gray-200 dark:bg-gray-700 justify-center items-center">
             <div className="text-center">
                 <h1 className="text-5xl font-semibold text-gray-400">Tailwind x React</h1>
-                <h2><a className="text-gray-500"
+                <h2 className="dark:text-gray-900"><a className="text-gray-500 dark:text-gray-300 font-semibold"
                     href="https://github.com/RamiroG8k" target="_blank" rel="noreferrer">RamiroG8k</a> template</h2>
             </div>
 
--- src/components/shared/ModeSwitcher.tsx
+++ src/components/shared/ModeSwitcher.tsx
@@ -13,10 +13,10 @@ const ModeSwitcher = (props: any) => {
     return (
         <Switch.Group>
             <div className={`${className} flex items-center`}>
-                <Switch.Label className="mr-4">Enable {enabled ? 'Light' : 'Dark'} Mode</Switch.Label>
+                <Switch.Label className="mr-4 dark:text-gray-400">Enable {enabled ? 'Light' : 'Dark'} Mode</Switch.Label>
                 <Switch checked={enabled} onChange={handleSwitch}
-                    className={`${enabled ? 'bg-blue-600' : 'bg-gray-200'
-                        } relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
+                    className={`${enabled ? 'bg-gray-900' : 'bg-gray-300'
+                        } relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none`}>
                     <span className={`${enabled ? 'translate-x-6' : 'translate-x-1'
                         } inline-block w-4 h-4 transform bg-white rounded-full transition-transform`} />
                 </Switch>
