9bbc9e93c216a166febb45bbe38eceebe633489c
Ramiro Mendez <ramiromendez@XIFRA-SIS006s-MacBook-Pro.local>
2021-09-20 17:17:35 -0500

    Modeswitcher standalaone

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
 import { ModeSwitcher } from 'components/shared';
 
 function App() {
-    const html = document.getElementsByTagName('html')[0];    
-
-    const toggleDarkMode = () => {
-        if (html.classList.contains('scheme-dark')) {
-            html.classList.remove('scheme-dark');
-        } else {
-            html.classList.add('scheme-dark');
-        }
-    }
-
     return (
         <section className="flex flex-col h-screen w-screen bg-gray-200 dark:bg-gray-700 justify-center items-center">
             <div className="text-center">
@@ -19,7 +9,7 @@ function App() {
                     href="https://github.com/RamiroG8k" target="_blank" rel="noreferrer">RamiroG8k</a> template</h2>
             </div>
 
-            <ModeSwitcher className="my-4" onSwitch={toggleDarkMode}/>
+            <ModeSwitcher />
         </section>
     );
 }
--- src/components/shared/ModeSwitcher.tsx
+++ src/components/shared/ModeSwitcher.tsx
@@ -1,28 +1,31 @@
-import { useState } from 'react'
-import { Switch } from '@headlessui/react'
+import { useState } from 'react';
+import { Switch } from '@headlessui/react';
+import { FiMoon, FiSun } from 'react-icons/fi';
 
-const ModeSwitcher = (props: any) => {
-    const { onSwitch, className } = props;
-    const [enabled, setEnabled] = useState(false);
+const ModeSwitcher = () => {
+    const [enabled, setEnabled] = useState<boolean>(false);
+    const htmlClasses: any = document.getElementsByTagName('html')[0].classList;
 
-    const handleSwitch = (state: boolean) => {
-        setEnabled(state);
-        onSwitch(state);
+    const toggleMode = () => {
+        console.log(htmlClasses);
+        
+        setEnabled(!enabled);
+        htmlClasses.contains('scheme-dark') ?
+            htmlClasses.remove('scheme-dark') :
+            htmlClasses.add('scheme-dark');
     };
 
     return (
-        <Switch.Group>
-            <div className={`${className} flex items-center`}>
-                <Switch.Label className="mr-4 dark:text-gray-400">Enable {enabled ? 'Light' : 'Dark'} Mode</Switch.Label>
-                <Switch checked={enabled} onChange={handleSwitch}
-                    className={`${enabled ? 'bg-gray-900' : 'bg-gray-300'
-                        } relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none`}>
-                    <span className={`${enabled ? 'translate-x-6' : 'translate-x-1'
-                        } inline-block w-4 h-4 transform bg-white rounded-full transition-transform`} />
-                </Switch>
-            </div>
+        <Switch.Group as="div" className="flex relative">
+            <Switch checked={enabled} onChange={toggleMode}
+                className={`${enabled ? 'bg-gray-800' : 'bg-gray-300'} relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none`}>
+                <span className={`${enabled ? 'translate-x-6' : 'translate-x-1'} inline-block w-4 h-4 transform bg-white dark:bg-gray-500 rounded-full transition-transform`} />
+            </Switch>
+
+            {enabled ? <span className="absolute top-1 left-1 text-gray-400"><FiMoon /></span> :
+                <span className="absolute top-1 right-1 text-white"><FiSun /></span>}
         </Switch.Group>
-    )
+    );
 };
 
 export default ModeSwitcher;
